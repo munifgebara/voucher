036ee2254917a8526469298f95376d34980234a1
Munif <munifgebara@gmail.com>
2020-06-10 18:23:18 -0300

    vouchers 0.1

--- src/main/java/br/com/munif/treinamento/unicesumar/voucher/bootstrap/Seed.java
+++ src/main/java/br/com/munif/treinamento/unicesumar/voucher/bootstrap/Seed.java
@@ -1,5 +1,8 @@
 package br.com.munif.treinamento.unicesumar.voucher.bootstrap;
 
+import java.util.HashSet;
+import java.util.Set;
+
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,7 +14,11 @@ import br.com.munif.framework.vicente.core.RightsHelper;
 import br.com.munif.framework.vicente.core.VicThreadScope;
 import br.com.munif.framework.vicente.security.seed.SeedSecurity;
 import br.com.munif.treinamento.unicesumar.voucher.entidades.Aluno;
+import br.com.munif.treinamento.unicesumar.voucher.entidades.CampanhaVoucher;
+import br.com.munif.treinamento.unicesumar.voucher.entidades.Candidato;
+import br.com.munif.treinamento.unicesumar.voucher.entidades.Concurso;
 import br.com.munif.treinamento.unicesumar.voucher.entidades.Polo;
+import br.com.munif.treinamento.unicesumar.voucher.entidades.Voucher;
 import br.com.munif.treinamento.unicesumar.voucher.repositorios.*;
 
 
@@ -66,6 +73,34 @@ public class Seed implements ApplicationListener<ContextRefreshedEvent> {
 		Polo polo1 = poloRepository.save(new Polo("Maringá"));
 		Polo polo2 = poloRepository.save(new Polo("Londrina"));
 		Polo polo3 = poloRepository.save(new Polo("Curitiba"));
+		
+		Set<Polo> polos = new HashSet<>();
+		polos.add(polo1);
+		polos.add(polo2);
+		
+		Concurso concurso1 = concursoRepository.save(new Concurso(2020, 2));
+		Concurso concurso2 = concursoRepository.save(new Concurso(2021, 1));
+		
+		Set<Concurso> concursos=new HashSet<>();
+		concursos.add(concurso1);
+		
+		
+		
+		
+		
+		CampanhaVoucher campanhaVoucher = campanhaVoucherRepository.save(new CampanhaVoucher("2020-2",concursos, polos));
+		
+		
+		Voucher voucher1=voucherRepository.save(new Voucher("VFO1", aluno1, campanhaVoucher));
+		Voucher voucher2=voucherRepository.save(new Voucher("VFO2", aluno1, campanhaVoucher));
+		Voucher voucher3=voucherRepository.save(new Voucher("VFA1", aluno2, campanhaVoucher));
+		
+		Candidato candidato1=candidatoRepository.save(new Candidato("Fulano de TAL", "111111111-01", "Sistemas", polo1, voucher1, true, concurso1));
+		Candidato candidato2=candidatoRepository.save(new Candidato("Fulana de OUTRO", "22222222-01", "Sistemas", polo2, voucher2, false, concurso1));
+		Candidato candidato3=candidatoRepository.save(new Candidato("Fulano de MAL", "3333333-01", "Sistemas", polo2, null, true, concurso1));
+		
+		
+		
 		
 		
 		
