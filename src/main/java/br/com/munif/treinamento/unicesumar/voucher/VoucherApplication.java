b785c6cb2f02eb3d1c3f00e1cf5e7aa3df8ca0d3
Munif <munifgebara@gmail.com>
2020-06-10 16:21:45 -0300

    first commit

---
+++ src/main/java/br/com/munif/treinamento/unicesumar/voucher/VoucherApplication.java
@@ -0,0 +1,13 @@
+package br.com.munif.treinamento.unicesumar.voucher;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VoucherApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VoucherApplication.class, args);
+	}
+
+}
